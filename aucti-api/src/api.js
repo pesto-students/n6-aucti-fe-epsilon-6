6455f4a63b4dcbb7699fe4dc44a1dba93e1cae9f
Kirushan <kirushanbalakrishnan@gmail.com>
2021-08-19 13:31:53 +0530

    File stricture change admin folder

--- aucti-api/src/api.js
+++ aucti-api/src/api.js
@@ -1,21 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-let cors = require("cors");
-const serverless = require("serverless-http");
-const bodyParser = require("body-parser");
-let admin = require("firebase-admin");
+require('dotenv').config();
+const express = require('express');
+let cors = require('cors');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 
-const serviceAccount = require("../serviceAccountKey.json");
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    // databaseURL: "https://auctiweb-default-rtdb.firebaseio.com"
-  });
-}
-
-const productsRouter = require("./routes/products");
-const usersRouter = require("./routes/users");
+const productsRouter = require('./routes/products.routes');
+const usersRouter = require('./routes/users.routes');
 
 const app = express();
 const router = express.Router();
@@ -23,20 +13,20 @@ const router = express.Router();
 const allowedOrigin = process.env.ALLOWED_ORIGIN_URL;
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigin !== origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+	origin: function (origin, callback) {
+		if (allowedOrigin !== origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
 };
 
-router.use("/products", productsRouter);
-router.use("/users", usersRouter);
+router.use('/products', productsRouter);
+router.use('/users', usersRouter);
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use("/.netlify/functions/api", router);
+app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
--- aucti-api/src/services/users.js
+++ aucti-api/src/services/users.js
@@ -1,138 +1,104 @@
-const admin = require("firebase-admin");
+const { admin, db } = require('../util/admin');
+const users = db.collection('users');
 
-const users = admin.firestore().collection("users");
+exports.fetchAllUsers = () =>
+	new Promise((resolve, reject) => {
+		users
+			//   .orderBy("order", "asc")
+			.get()
+			.then((querySnapshot) => {
+				const data = querySnapshot?.docs?.map((doc) => ({
+					...doc.data(),
+					id: doc.id,
+				}));
+				resolve(data);
+			})
+			.catch((err) => {
+				let msg = 'Unable to retrieve categories';
+				reject(msg);
+			});
+	});
 
-const fetchAllUsers = () =>
-  new Promise((resolve, reject) => {
-    users
-      //   .orderBy("order", "asc")
-      .get()
-      .then((querySnapshot) => {
-        const data = querySnapshot?.docs?.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        resolve(data);
-      })
-      .catch((err) => {
-        let msg = "Unable to retrieve categories";
-        reject(msg);
-      });
-  });
+exports.addSeller = (createdAt, account_status, alias_name, address, phone_number, role, rating) =>
+	new Promise((resolve, reject) => {
+		const data = {
+			createdAt: admin.firestore().FieldValue.serverTimestamp(),
+			account_status: 'pending',
+			alias_name,
+			address,
+			phone_number,
+			role: 'seller',
+			rating,
+		};
+		users
+			.add(data)
+			.then((docRef) => resolve({ ...data, id: docRef.id }))
+			.catch(() => {
+				let msg = 'Unable to add the task';
+				reject(msg);
+			});
+	});
 
-const addSeller = (
-  createdAt,
-  account_status,
-  alias_name,
-  address,
-  phone_number,
-  role,
-  rating
-) =>
-  new Promise((resolve, reject) => {
-    const data = {
-      createdAt: admin.firestore().FieldValue.serverTimestamp(),
-      account_status: "pending",
-      alias_name,
-      address,
-      phone_number,
-      role: "seller",
-      rating,
-    };
-    users
-      .add(data)
-      .then((docRef) => resolve({ ...data, id: docRef.id }))
-      .catch(() => {
-        let msg = "Unable to add the task";
-        reject(msg);
-      });
-  });
+exports.addBuyer = (createdAt, account_status, alias_name, address, phone_number, role, rating) =>
+	new Promise((resolve, reject) => {
+		const data = {
+			createdAt: admin.firestore().FieldValue.serverTimestamp(),
+			account_status: 'approved',
+			alias_name,
+			address,
+			phone_number,
+			role: 'buyer',
+			rating,
+		};
+		users
+			.add(data)
+			.then((docRef) => resolve({ ...data, id: docRef.id }))
+			.catch(() => {
+				let msg = 'Unable to add the task';
+				reject(msg);
+			});
+	});
 
-const addBuyer = (
-  createdAt,
-  account_status,
-  alias_name,
-  address,
-  phone_number,
-  role,
-  rating
-) =>
-  new Promise((resolve, reject) => {
-    const data = {
-      createdAt: admin.firestore().FieldValue.serverTimestamp(),
-      account_status: "approved",
-      alias_name,
-      address,
-      phone_number,
-      role: "buyer",
-      rating,
-    };
-    users
-      .add(data)
-      .then((docRef) => resolve({ ...data, id: docRef.id }))
-      .catch(() => {
-        let msg = "Unable to add the task";
-        reject(msg);
-      });
-  });
+exports.addAdmin = (createdAt, account_status, alias_name, address, phone_number, role, rating) =>
+	new Promise((resolve, reject) => {
+		const data = {
+			createdAt: admin.firestore().FieldValue.serverTimestamp(),
+			account_status: 'approved',
+			alias_name,
+			address,
+			phone_number,
+			role: 'admin',
+			rating,
+		};
+		users
+			.add(data)
+			.then((docRef) => resolve({ ...data, id: docRef.id }))
+			.catch(() => {
+				let msg = 'Unable to add the task';
+				reject(msg);
+			});
+	});
 
-const addAdmin = (
-  createdAt,
-  account_status,
-  alias_name,
-  address,
-  phone_number,
-  role,
-  rating
-) =>
-  new Promise((resolve, reject) => {
-    const data = {
-      createdAt: admin.firestore().FieldValue.serverTimestamp(),
-      account_status: "approved",
-      alias_name,
-      address,
-      phone_number,
-      role: "admin",
-      rating,
-    };
-    users
-      .add(data)
-      .then((docRef) => resolve({ ...data, id: docRef.id }))
-      .catch(() => {
-        let msg = "Unable to add the task";
-        reject(msg);
-      });
-  });
+exports.deleteUser = (id) =>
+	new Promise((resolve, reject) => {
+		users
+			.doc(id)
+			.delete()
+			.then(() => resolve())
+			.catch(() => {
+				let msg = 'Unable to delete the category';
+				reject(msg);
+			});
+	});
 
-const deleteUser = (id) =>
-  new Promise((resolve, reject) => {
-    users
-      .doc(id)
-      .delete()
-      .then(() => resolve())
-      .catch(() => {
-        let msg = "Unable to delete the category";
-        reject(msg);
-      });
-  });
-
-const updateUser = (user) =>
-  new Promise((resolve, reject) => {
-    users
-      .doc(user.id)
-      .set({ ...user }, { merge: true })
-      .then(() => resolve())
-      .catch(() => {
-        let msg = "Unable to update category";
-        reject(msg);
-      });
-  });
-
-module.exports = {
-  fetchAllUsers,
-  addSeller,
-  addBuyer,
-  addAdmin,
-  deleteUser,
-  updateUser,
-};
+exports.updateUser = (user) =>
+	new Promise((resolve, reject) => {
+		users
+			.doc(user.id)
+			.set({ ...user }, { merge: true })
+			.then(() => resolve())
+			.catch(() => {
+				let msg = 'Unable to update category';
+				reject(msg);
+			});
+	});
