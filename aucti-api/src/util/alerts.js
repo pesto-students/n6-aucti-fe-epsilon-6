78c3eeb90855a1a5fdb9c8ad9de6d5540744233e
Kirushan <kirushanbalakrishnan@gmail.com>
2021-08-23 10:13:38 +0530

    Alert message on one file and export to entire project

---
+++ aucti-api/src/util/alerts.js
@@ -0,0 +1,43 @@
+module.exports.productServiceAlerts = {
+	UNABLE_TO_RETRIVE_PRODUCTS: 'Unable to retrieve Products!',
+	PRODUCT_ID_EMPTY: 'productId is empty',
+	UNABLE_TO_RETRIVE_PRODUCT: 'Unable to retrieve  product',
+	SELLER_ID_EMPTY: 'seller_id is empty',
+	UNABLE_TO_RETRIVE_SELLER_PRODUCTS: 'Unable to retrieve Seller products',
+};
+
+module.exports.auction_status = {
+	LIVE: 'live',
+	COMPLETED: 'completed',
+	HOLD: 'hold',
+	CENCELLED: 'cancelled',
+	DRAFT: 'draft',
+};
+
+module.exports.product_approval_status = {
+	APPROVED: 'approved',
+	PENDING: 'pending',
+	REJECTED: 'rejected',
+};
+
+module.exports.account_status = {
+	APPROVED: 'approved',
+	PENDING: 'pending',
+	REJECTED: 'rejected',
+};
+
+module.exports.role = {
+	BUYER: 'buyer',
+	SELLER: 'seller',
+	ADMIN: 'admin',
+};
+
+module.exports.notification_status = {
+	READ: 'read',
+	UNREAD: 'unread',
+};
+
+module.exports.offer_status = {
+	LIVE: 'live',
+	STOP: 'stop',
+};
