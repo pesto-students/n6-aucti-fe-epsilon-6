55befc29b8c14fd483171e21e6ce7324525e4cb1
Kirushan <kirushanbalakrishnan@gmail.com>
2021-08-25 12:35:31 +0530

    api update

--- aucti-api/src/util/alerts.js
+++ aucti-api/src/util/alerts.js
@@ -1,43 +1,10 @@
 module.exports.productServiceAlerts = {
-	UNABLE_TO_RETRIVE_PRODUCTS: 'Unable to retrieve Products!',
-	PRODUCT_ID_EMPTY: 'productId is empty',
-	UNABLE_TO_RETRIVE_PRODUCT: 'Unable to retrieve  product',
-	SELLER_ID_EMPTY: 'seller_id is empty',
-	UNABLE_TO_RETRIVE_SELLER_PRODUCTS: 'Unable to retrieve Seller products',
-};
-
-module.exports.auction_status = {
-	LIVE: 'live',
-	COMPLETED: 'completed',
-	HOLD: 'hold',
-	CENCELLED: 'cancelled',
-	DRAFT: 'draft',
-};
-
-module.exports.product_approval_status = {
-	APPROVED: 'approved',
-	PENDING: 'pending',
-	REJECTED: 'rejected',
-};
-
-module.exports.account_status = {
-	APPROVED: 'approved',
-	PENDING: 'pending',
-	REJECTED: 'rejected',
-};
-
-module.exports.role = {
-	BUYER: 'buyer',
-	SELLER: 'seller',
-	ADMIN: 'admin',
-};
-
-module.exports.notification_status = {
-	READ: 'read',
-	UNREAD: 'unread',
-};
-
-module.exports.offer_status = {
-	LIVE: 'live',
-	STOP: 'stop',
+	UNABLE_TO_RETRIVE_PRODUCTS: "Unable to retrieve Products!",
+	PRODUCT_ID_EMPTY: "productId is empty",
+	UNABLE_TO_RETRIVE_PRODUCT: "Unable to retrieve  product",
+	SELLER_ID_EMPTY: "seller_id is empty",
+	UNABLE_TO_RETRIVE_SELLER_PRODUCTS: "Unable to retrieve Seller products",
+	NO_IMAGE_FILE: "No image file",
+	IMAGE_UPLOAD_ERROR: "Something is wrong! Unable to upload at the moment.",
+	UNABLE_TO_ADD_PRODUCT: "Unable to add the Product",
 };
