e5549f539eb90e93fae2b0ce280aca144f0caee3
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-01 20:18:59 +0530

    remove gitignore files

--- aucti-web/build/static/js/5.27e41258.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[5],{90:function(t,i,n){"use strict";n.r(i);n(0);var c=n(1);i.default=function(){return Object(c.jsx)("div",{children:"BuyerNotifications works!"})}}}]);
-//# sourceMappingURL=5.27e41258.chunk.js.map
--- aucti-web/build/static/js/6.21529f45.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[6],{92:function(t,e,i){"use strict";i.r(e);i(0);var n=i(1);e.default=function(){return Object(n.jsx)("div",{children:"Profile works!"})}}}]);
-//# sourceMappingURL=6.21529f45.chunk.js.map
