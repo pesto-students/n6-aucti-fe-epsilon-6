229194bc7b74fb32a3c7ecec601027799cb6ae42
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-03 22:40:14 +0530

    Merge with master

--- aucti-web/src/redux/reducers/rootReducer.js
+++ aucti-web/src/redux/reducers/rootReducer.js
@@ -1,12 +1,44 @@
 import { combineReducers } from "redux";
-import { productReducer } from "./productReducer";
 import { productsReducer } from "./productsReducer";
 import { wishlistReducer } from "./wishlistReducer"
-import { bidReducer } from "./bidReducer"
+import { userReducer } from "./userReducer";
+import { alertReducer } from "./alertReducer";
+import { bidReducer } from "./bidReducer";
+import { buyerBidReducer } from "./buyerBidReducer";
+import { buyerInsightsReducer } from "./buyerInsightsReducer";
+import { buyerWishlistReducer } from "./buyerWishlistReducer";
+import { buyerHistoryReducer } from "./buyerHistoryReducer";
+import { buyerNotificationReducer } from "./buyerNotificationReducer";
+import { addressReducer } from "./addressReducer";
+import { sellerProductReducer } from "./sellerProductReducer";
+import { sellerInsightsReducer } from "./sellerInsightsReducer";
+import { sellerAddProductReducer } from "./sellerAddProductReducer";
+import { sellerBidsUserReducer } from "./sellerBidsUserReducer";
+import { paymentReducer } from "./paymentReducer";
+import { sellerHistoryReducer } from "./sellerHistoryReducer";
+import { sellerCompletedReducer } from "./sellerCompletedReducer";
+import { buyerCompletedReducer } from "./buyerCompletedReducer";
+
 const rootReducer = combineReducers({
+  products: productReducer,
+  user: userReducer,
+  alert: alertReducer,
+  bids: bidReducer,
+  buyerBids: buyerBidReducer,
+  insights: buyerInsightsReducer,
+  wishlist: buyerWishlistReducer,
+  buyerHistory: buyerHistoryReducer,
+  notifications: buyerNotificationReducer,
+  addresses: addressReducer,
+  sellerProducts: sellerProductReducer,
+  sellerInsights: sellerInsightsReducer,
+  addProductUpdate: sellerAddProductReducer,
+  bidsWithUsers: sellerBidsUserReducer,
+  paymentMessage: paymentReducer,
+  sellerHistory: sellerHistoryReducer,
+  sellerCompleted: sellerCompletedReducer,
+  buyerCompleted: buyerCompletedReducer,
   productsReducer,
-  productReducer,
   wishlistReducer,
-  bidReducer
 });
 export default rootReducer;
