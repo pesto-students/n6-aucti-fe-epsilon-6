8a0436bc155afbfda87b9098e950c79238e7c08a
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-05 18:43:08 +0530

    after merge

--- aucti-web/src/redux/saga/rootSaga.js
+++ aucti-web/src/redux/saga/rootSaga.js
@@ -3,22 +3,19 @@ import { buyerSaga } from "./buyerSaga";
 import { sellerSaga } from "./sellerSaga";
 import { userSaga } from "./userSaga";
 import { wishlistSaga } from "./wishlistSaga";
-import {productsSaga} from './productsSaga'
-import {productSaga} from './productSaga'
-import { wishlistSaga } from './wishlistSaga'
-import { bidSaga } from './bidSaga'
-
-
+import { productsSaga } from "./productsSaga";
+import { productSaga } from "./productSaga";
+import { bidSaga } from "./bidSaga";
 
 export default function* rootSaga() {
-    yield all([
-        productsSaga(),
-        productSaga(),
-        wishlistSaga(),
-        productSaga(),
-        userSaga(),
-        buyerSaga(),
-        sellerSaga(),
-        bidSaga(),
-    ]);
+	yield all([
+		productsSaga(),
+		productSaga(),
+		wishlistSaga(),
+		productSaga(),
+		userSaga(),
+		buyerSaga(),
+		sellerSaga(),
+		bidSaga(),
+	]);
 }
