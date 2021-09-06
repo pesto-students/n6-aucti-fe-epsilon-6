23b7f0a6f9fcf6def567f3920c8510bc11b10f4d
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-06 11:59:13 +0530

    Product page layout and responsive update

--- aucti-web/src/components/Pages/ProductPage/ProductPage.js
+++ aucti-web/src/components/Pages/ProductPage/ProductPage.js
@@ -16,7 +16,7 @@ const ProductPage = (props) => {
 	if (productdata !== undefined) {
 		return (
 			<>
-				<div className="col-start-1 row-start-2 col-span-1 row-span-4 ">
+				{/* <div className="col-start-1 row-start-2 col-span-1 row-span-4 ">
 					<h1></h1>
 				</div>
 				<div className="col-start-1 row-start-1 col-span-5 row-span-4 flex justify-center">
@@ -31,6 +31,72 @@ const ProductPage = (props) => {
 						product_picture={productdata.product_picture}
 						id={props.match.params.id}
 					/>
+				</div> */}
+
+				<div className="flex xl:flex-row xs:flex-col bg-white dark:bg-gray-800 rounded-lg shadow p-12 m-8">
+					<div className="flex-none xl:w-64 xs:w-48 xl:mr-12">
+						<img
+							src={productdata.product_picture}
+							alt="shopping image"
+							className=" rounded-lg inset-0 h-full object-cover"
+						/>
+					</div>
+					<form className="flex-auto">
+						<div className="flex flex-col">
+							<h1 className="flex-auto text-2xl font-semibold dark:text-gray-50">
+								{productdata.title}
+							</h1>
+							<div className="flex flex-col mt-4">
+								<div className="text-xl font-semibold text-gray-900 dark:text-gray-300">
+									{"₹" + productdata.base_price}
+								</div>
+								<div className="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300">
+									Base Price
+								</div>
+							</div>
+							<div className="flex flex-col mt-4">
+								<div className="text-xl font-semibold text-gray-900 dark:text-gray-300">
+									{"₹" + productdata.base_price}
+								</div>
+								<div className="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 ">
+									Highest Bid
+								</div>
+							</div>
+							<div className="flex xl:flex-row xs:flex-col justify-start mt-4">
+								<div className="flex-none text-lg font-medium text-gray-500 dark:text-gray-300 mr-2 ">
+									Number of bids registered:
+								</div>
+								<div className="text-lg font-semibold text-gray-900 dark:text-gray-300">
+									{productdata.base_price}
+								</div>
+							</div>
+							<div className="flex flex-col mt-4">
+								<div className="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 ">
+									Seller:
+								</div>
+							</div>
+						</div>
+
+						<div className="flex xl:flex-row xs:flex-col justify-between mb-4 text-sm font-medium mt-4">
+							<button
+								type="button"
+								className="py-2 px-4 mr-10 bg-aucti hover:bg-auctiHover focus:ring-indigo-500 focus:ring-offset-indigo-200 text-gray-900 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded "
+								// onClick={selectHighestBidder}
+							>
+								Bid Now
+							</button>
+							<button
+								type="button"
+								className="xl:mt-0 xs:mt-4 py-2 px-4  bg-aucti hover:bg-auctiHover focus:ring-indigo-500 focus:ring-offset-indigo-200 text-gray-900 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded "
+								// onClick={selectHighestBidder}
+							>
+								Add To Wishlist
+							</button>
+						</div>
+						<p className="text-sm text-gray-500 dark:text-gray-300">
+							{productdata.description}
+						</p>
+					</form>
 				</div>
 			</>
 		);
