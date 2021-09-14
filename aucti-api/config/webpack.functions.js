2c1f3f048625375f91837bba3ca1b50558be66cb
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-14 23:58:31 +0530

    Api authentication header update

--- aucti-api/config/webpack.functions.js
+++ aucti-api/config/webpack.functions.js
@@ -1,9 +1,5 @@
 const nodeExternals = require("webpack-node-externals");
 
-const Dotenv = require("dotenv-webpack");
-
-// @see https://github.com/netlify/netlify-lambda#webpack-configuration
 module.exports = {
 	externals: [nodeExternals()],
-	plugins: [new Dotenv()],
 };
