c18ed36b3fd850755d751b3027cb2ecc9edb2d21
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-14 16:59:34 +0530

    api authentication

--- aucti-api/config/webpack.functions.js
+++ aucti-api/config/webpack.functions.js
@@ -1,5 +1,9 @@
 const nodeExternals = require("webpack-node-externals");
 
+const Dotenv = require("dotenv-webpack");
+
+// @see https://github.com/netlify/netlify-lambda#webpack-configuration
 module.exports = {
 	externals: [nodeExternals()],
+	plugins: [new Dotenv()],
 };
