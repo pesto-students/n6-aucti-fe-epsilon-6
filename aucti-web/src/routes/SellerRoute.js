f02e1a66a447bd7211ca49fe7e4edbefd5983285
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-15 01:17:14 +0530

    Prittier and eslint pre commit hook

--- aucti-web/src/routes/SellerRoute.js
+++ aucti-web/src/routes/SellerRoute.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Route, Redirect } from "react-router-dom";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
 
-const SellerRoute = (props) => {
-	if (props.user.role === "seller") {
-		return <Route {...props} />;
-	}
-	return <Redirect to="/" />;
+const SellerRoute = props => {
+  if (props.user.role === 'seller') {
+    return <Route {...props} />;
+  }
+  return <Redirect to="/" />;
 };
 
 export default connect(({ user }) => ({ user }))(SellerRoute);
