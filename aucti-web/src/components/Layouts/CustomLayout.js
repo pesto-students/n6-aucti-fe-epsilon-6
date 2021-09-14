132cba1722882e640484554a84f26fafab230a5d
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-15 00:09:33 +0530

    Prittier and eslint pre commit hook

--- aucti-web/src/components/Layouts/CustomLayout.js
+++ aucti-web/src/components/Layouts/CustomLayout.js
@@ -1,45 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
-import Sidebar from "../Layouts/Sidebar/MainSidebar";
-import Header from "../Pages/Dashboards/Header";
-import Main from "../Pages/Dashboards/Main";
+import Sidebar from '../Layouts/Sidebar/MainSidebar';
+import Header from '../Pages/Dashboards/Header';
+import Main from '../Pages/Dashboards/Main';
 
 function CustomLayout(props) {
-	// const { user } = props;
-	const [isSidebarOpen, closeSidebar] = useState(false);
-	let location = useLocation();
+  // const { user } = props;
+  const [isSidebarOpen, closeSidebar] = useState(false);
+  let location = useLocation();
 
-	useEffect(() => {
-		closeSidebar(true);
-	}, [location]);
+  useEffect(() => {
+    closeSidebar(true);
+  }, [location]);
 
-	return (
-		<div
-			className={`flex h-screen bg-gray-50 dark:bg-gray-900
-				${isSidebarOpen && "overflow-hidden"}`}
-		>
-			<div className="flex flex-col flex w-screen">
-				<Header />
+  return (
+    <div
+      className={`flex h-screen bg-gray-50 dark:bg-gray-900
+				${isSidebarOpen && 'overflow-hidden'}`}>
+      <div className="flex flex-col  w-screen">
+        <Header />
 
-				<div className="flex flex-row ">
-					<Sidebar />
-					<Main>{props.children}</Main>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="flex flex-row ">
+          <Sidebar />
+          <Main>{props.children}</Main>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-	return {
-		user: state.user,
-	};
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+  };
 };
 
 const mapDispatchToProps = () => {
-	return {};
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CustomLayout);
