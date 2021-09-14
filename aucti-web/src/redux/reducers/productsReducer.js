429e1d92c3c0bf61c375594d0507d9d769374b07
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-15 01:17:58 +0530

    Prittier and eslint pre commit hook

--- aucti-web/src/redux/reducers/productsReducer.js
+++ aucti-web/src/redux/reducers/productsReducer.js
@@ -1,12 +1,12 @@
-import * as types from "../types";
+import * as types from '../types';
 //have an object instead of array
 const initialState = [];
 
 export const productsReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case types.PRODUCTS_LOADED:
-			return action.products;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case types.PRODUCTS_LOADED:
+      return action.products;
+    default:
+      return state;
+  }
 };
