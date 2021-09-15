bf3e60de4d1d43ac6d9593d23f973a9536339e67
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-15 18:24:29 +0530

    Prittier and eslint precommit

--- src/components/Tests/ProductPage/ProductPage.test.js
+++ src/components/Tests/ProductPage/ProductPage.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ProductPage from '../../Pages/ProductPage/ProductPage';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import ProductPage from '../../Pages/ProductPage/ProductPage';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
